cb4353e54e7cf20607a52df02905d73c38991a1e
Bobislav <bobo.svilenov@gmail.com>
2024-02-19 13:15:38 +0200

    commiting all

--- app/sketch.js
+++ app/sketch.js
@@ -1,14 +1,18 @@
 const socket = io(`http://${window.location.hostname}:8080`); 
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(900, 900);
   background(0);
+  stroke(255, 255, 255);
+  rect(300, 0, 10, 900);
+  rect(600, 0, 10, 900);
 }
 
 function draw() {
   socket.on("update", msg => {
     background(0);
-
+    rect(300, 0, 10, 900);
+    rect(600, 0, 10, 900);
   });
 }
 
